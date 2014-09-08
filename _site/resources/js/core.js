bd5f24315fa9b27c5f8c60ea84dc3ad395bf5f54
张嘉杰 <zj7687362@gmail.com>
2014-09-08 16:13:29 +0800

    blog

--- _site/resources/js/core.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	NProgress.start();
-	
-	$(window).scroll(function() { if($(window).scrollTop() >= 100){ $('.topfade').fadeIn(300); }else{ $('.topfade').fadeOut(300); } });
-
-	$('.topfade').click(function(){ $('html,body').animate({scrollTop: '0px'}, 800);});
-	
-	NProgress.done();
-});
