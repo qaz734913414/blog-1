9086cae173789959293ab1851e3b8c5bfa8b2582
张嘉杰 <zj7687362@gmail.com>
2015-01-02 01:51:32 +0800

    update

--- resources/demo/2015/01/01/unlock/js/mazelock.js
+++ resources/demo/2015/01/01/unlock/js/mazelock.js
@@ -362,12 +362,19 @@ var MazeLock = (function(global, doc){
             element.removeEventListener(_touchMove, MoveListener, false);
             element.removeEventListener(_touchEnd,  EndListener,  false);
         }
+        
+        function unListener(){
+        	// 移除移动和离开事件
+            element.removeEventListener(_touchMove, MoveListener, false);
+            element.removeEventListener(_touchEnd,  EndListener,  false);
+        }
 
         /* 返回监听函数 */
         return {
             StartListener : StartListener,
             MoveListener  : MoveListener,
-            EndListener   : EndListener
+            EndListener   : EndListener,
+            UnListener	  : unListener
         }
     }
 
@@ -416,6 +423,7 @@ var MazeLock = (function(global, doc){
                 _circles.DrawClearCircle();
             },
             drawErrorPanel : function(){
+            	_listeners.UnListener();
                 _circles.DrawErrorCircle(function(){
                 	setTimeout(function(){
                         _circles.DrawClearCircle();
