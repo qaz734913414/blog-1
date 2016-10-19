3f36f708ef6e45c50c4c244f15a7bd1eb0bd8112
张嘉杰 <zj7687362@gmail.com>
2016-10-19 14:26:49 +0800

    update

--- resources/duoshuo/duoshuo.js
+++ resources/duoshuo/duoshuo.js
@@ -1124,7 +1124,8 @@ function os(e) {
                     title: "\u65b0\u7559\u8a00\u53ca\u56de\u590d",
                     apiUrl: "/api/users/unreadComments",
                     tmpl: function(e) {
-                        return '<li data-thread-id="' + e.thread.thread_id + '">' + i.map(e.authors, J.userAnchor).join("\u3001") + ' \u5728 <a class="ds-read" href="' + e.thread.url.substring(0,e.thread.url.length-1) + '#comments" target="_blank">' + p(e.thread.title || "\u65e0\u6807\u9898") + '</a> \u4e2d\u56de\u590d\u4e86\u4f60 <a class="ds-delete ds-read" title="\u77e5\u9053\u4e86" href="javascript:void(0)">\u77e5\u9053\u4e86</a></li>'
+                    	var _url = e.thread.url.substring(0,((t.charAt(t.length-1)!="/")?e.thread.url:(e.thread.url.length-1))) + "#comments";
+                        return '<li data-thread-id="' + e.thread.thread_id + '">' + i.map(e.authors, J.userAnchor).join("\u3001") + ' \u5728 <a class="ds-read" href="' + _url + '" target="_blank">' + p(e.thread.title || "\u65e0\u6807\u9898") + '</a> \u4e2d\u56de\u590d\u4e86\u4f60 <a class="ds-delete ds-read" title="\u77e5\u9053\u4e86" href="javascript:void(0)">\u77e5\u9053\u4e86</a></li>'
                     },
                     read: function(e) {
                         var t = e.attr("data-thread-id");
@@ -1882,8 +1883,8 @@ function os(e) {
             return i.map(e,
             function(e) {
                 var n = K(e);
-                console.log(e.thread.url);
-                return '<li class="ds-comment' + (t.show_avatars ? " ds-show-avatars": "") + '" data-post-id="' + e.post_id + '">' + (t.show_avatars ? '<div class="ds-avatar">' + J.avatar(n, t.avatar_size) + "</div>": "") + '<div class="ds-meta">' + J.userAnchor(n) + (t.show_time ? J.timeHtml(e.created_at) : "") + "</div>" + (t.show_title ? '<div class="ds-thread-title">\u5728 <a href="' + p(e.thread.url.substring(0,((t.charAt(t.length-1)!="/")?e.thread.url:(e.thread.url.length-1))) + "#comments") + '">' + p(e.thread.title) + '</a> \u4e2d\u8bc4\u8bba</div><div class="ds-excerpt">' + e.message + "</div>": '<a class="ds-excerpt" title="' + p(e.thread.title) + ' \u4e2d\u7684\u8bc4\u8bba" href="' + p(e.thread.url.substring(0,((t.charAt(t.length-1)!="/")?e.thread.url:(e.thread.url.length-1))) + "#comments") + '">' + e.message + "</a>") + "</li>"
+                var _url = p(e.thread.url.substring(0,((t.charAt(t.length-1)!="/")?e.thread.url:(e.thread.url.length-1))) + "#comments");
+                return '<li class="ds-comment' + (t.show_avatars ? " ds-show-avatars": "") + '" data-post-id="' + e.post_id + '">' + (t.show_avatars ? '<div class="ds-avatar">' + J.avatar(n, t.avatar_size) + "</div>": "") + '<div class="ds-meta">' + J.userAnchor(n) + (t.show_time ? J.timeHtml(e.created_at) : "") + "</div>" + (t.show_title ? '<div class="ds-thread-title">\u5728 <a href="' + _url + '">' + p(e.thread.title) + '</a> \u4e2d\u8bc4\u8bba</div><div class="ds-excerpt">' + e.message + "</div>": '<a class="ds-excerpt" title="' + p(e.thread.title) + ' \u4e2d\u7684\u8bc4\u8bba" href="' + _url + '">' + e.message + "</a>") + "</li>"
             }).join("")
         },
         H.RecentComments = R.extend({
